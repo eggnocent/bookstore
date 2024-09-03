c8ba9bbaed07decdb680282604ce2f8a84612d94
eggnocent <egiwiratamaaa@gmail.com>
2024-09-03 10:24:18 +0700

    add func generate jwt

--- middlewares/jwt_helper.go
+++ middlewares/jwt_helper.go
@@ -2,6 +2,7 @@ package middlewares
 
 import (
 	"apimandiri/config"
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -14,4 +15,13 @@ func GenerateJWT(username string) (string, error) {
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(config.JWTSecretkey)
+}
+
+func VerifyJWT(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("metode signing tida sesuai")
+		}
+		return config.JWTSecretkey, nil
+	})
 }
